0916657a7c3a4e06e9852cf7226941f9d935e81e
odedbe15 <odedb0105@gmail.com>
2025-01-16 16:35:58 +0200

    Started Working On Elevator Subsystem

---
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorIO.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.Elevator;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ElevatorIO {
+ 
+    @AutoLog
+    public static class ElevatorIOInputs  {
+        
+        boolean motorConnected = false;
+        double elevatorVelocity = 0.0;
+        double elevatorPosition = 0.0;
+        double elevatorAppliedVolts = 0.0;
+        double elevatorCurrentAmps = 0.0;
+        
+        
+    }
+
+    public default void updateInputs(ElevatorIOInputs inputs) {}
+}
---
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorSubsystem.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.Elevator;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ElevatorSubsystem extends SubsystemBase {
+    
+}
