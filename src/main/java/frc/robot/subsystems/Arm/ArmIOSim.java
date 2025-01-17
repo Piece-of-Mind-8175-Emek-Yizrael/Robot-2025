802bae951b49ce1b2ddbbc51b800baaba185827d
odedbe15 <odedb0105@gmail.com>
2025-01-17 19:25:44 +0200

    Added SimulationIO and commented on some files

---
+++ src/main/java/frc/robot/subsystems/Arm/ArmIOSim.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.Arm;
+
+public class ArmIOSim implements ArmIO {
+
+    
+    public ArmIOSim() {
+    }
+
+    @Override
+    public void setDegrees(int Degrees) {
+    }
+
+    @Override
+    public void updateInputs(ArmIOInputs inputs) {
+    }
+    
+}
