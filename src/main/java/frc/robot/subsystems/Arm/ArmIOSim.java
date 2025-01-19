147a67d8a29f1182f6174870e9927e83144137d2
odedbe15 <odedb0105@gmail.com>
2025-01-19 17:05:37 +0200

    Added Pose To ArmIOSim

--- src/main/java/frc/robot/subsystems/Arm/ArmIOSim.java
+++ src/main/java/frc/robot/subsystems/Arm/ArmIOSim.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems.Arm;
 
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.wpilibj.simulation.PWMSim;
 
 public class ArmIOSim implements ArmIO {
@@ -7,14 +11,19 @@ public class ArmIOSim implements ArmIO {
     private PWMSim sim;
 
     private final double MAX_ANGLE = 180;
+
+    private Pose3d pose;
     public ArmIOSim() {
         sim = new PWMSim(0);//TODO Replace with port constant
+        Logger.recordOutput("Arm/Pose", pose);
 
     }
 
     @Override
     public void setDegrees(Double Degrees) {
         sim.setPosition(Degrees);
+        pose = new Pose3d(0,0,0, new Rotation3d(0,getAngle(),0));
+
 
     }
 
