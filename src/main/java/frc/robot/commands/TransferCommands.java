4464d034df84b5c8f6ae506c41bdd3a16ddccb1e
ilaybaruch <166049550+ilaybaruch@users.noreply.github.com>
2025-01-19 17:28:54 +0200

    finished commands

--- src/main/java/frc/robot/commands/TransferCommands.java
+++ src/main/java/frc/robot/commands/TransferCommands.java
@@ -1,22 +1,21 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.Transfer.Transfer;
+import edu.wpi.first.wpilibj2.command.Commands;
 import static frc.robot.subsystems.Transfer.TransferConstants.CORAL_INTAKE_SPEED;
 import static frc.robot.subsystems.Transfer.TransferConstants.CORAL_OUTTAKE_SPEED;
+import frc.robot.subsystems.Transfer.Transfer;
 
 
-public class TransferCommands extends SubsystemBase {
-Transfer transfer;
+public class TransferCommands{
+          
+        public static Command startTransfer(Transfer transfer) {
+            return Commands.startEnd(() -> transfer.setSpeed(CORAL_INTAKE_SPEED), () ->transfer.stopMotor()).until(() -> transfer.getIO().isCoralIn());
+        }
+        
+        
+        public static Command coralOutake(Transfer transfer) {
+            return Commands.startEnd(() -> transfer.setSpeed(CORAL_OUTTAKE_SPEED), () -> transfer.stopMotor()).until(() -> !transfer.getIO().isCoralIn());
+        }
+    }
 
-    public Command startTransfer() {
-        return startEnd(() -> transfer.setSpeed(CORAL_INTAKE_SPEED), () ->transfer.stopMotor()).until(() -> transfer.getIO().isCoralIn());
-            }
-    
-
-            public Command coralOutake() {
-                return startEnd(() -> transfer.setSpeed(CORAL_OUTTAKE_SPEED), () -> transfer.stopMotor()).until(() -> !transfer.getIO().isCoralIn());
-            }
-            
-}
