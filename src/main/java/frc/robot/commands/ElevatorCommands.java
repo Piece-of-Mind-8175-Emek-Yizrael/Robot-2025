a48bf57fa5cdafaa17034572fc9769b0b56ae2ec
gil-liGrota <gilgrota@gmail.com>
2025-01-22 16:20:18 +0200

    starting commnd

---
+++ src/main/java/frc/robot/commands/ElevatorCommands.java
@@ -0,0 +1,21 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.Elevator.ElevatorSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class ElevatorCommands {
+
+
+    // public static Command goToPosition(double position){
+    //     return run(() -> setSetPoint(position)).until(atGoal());
+    // }
+    
+    // public static Command stopElevator(){
+    //     return runOnce(() -> resistGravity());
+    // }
+
+    public static Command goToPosition(ElevatorSubsystem elevator)
+    
+}
